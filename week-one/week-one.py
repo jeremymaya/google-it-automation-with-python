8cd7c5af13f0351ac9179ff0358d08c1b5858933
Kyungrae <jeremiah.kim@hotmail.com>
2020-01-20 10:25:10 -0800

    print statement added

--- week-one/week-one.py
+++ week-one/week-one.py
@@ -2,7 +2,12 @@ friends = ['Taylor', 'Alex', 'Pat', 'Eli']
 for friend in friends:
     print("Hi " + friend)
 
-print("")
+print("---------------------------------")
 
 for i in range(10):
     print("Hello, World!")
+
+print("---------------------------------")
+
+# Fill in the correct Python comman to put "My first Python program" onto the screen.
+print("My first Python pgorgram")
