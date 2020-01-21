8d5cc36826a0b35af360c3eafecb88a7e14e9aa8
Kyungrae <jeremiah.kim@hotmail.com>
2020-01-21 14:58:51 -0800

    the principle of code reuse completed

--- crash-course-on-python/week-two/course-one-week-two.py
+++ crash-course-on-python/week-two/course-one-week-two.py
@@ -55,3 +55,26 @@ def convert_seconds(seconds):
 
 hours, minutes, seconds = convert_seconds(5000)
 print(hours, minutes, seconds)
+
+# The Principles of Code Reuse
+"""
+In this code, identify the repeated pattern and replace it with a function called print_monthly_expense, that receives the name of the month and the hours as parameters. Adapt the rest of the code so that the result is the same.
+"""
+june_hours = 243
+june_cost = june_hours * 0.65
+print("In June we spent: " + str(june_cost))
+
+july_hours = 325
+july_cost = july_hours * 0.65
+print("In July we spent: " + str(july_cost))
+
+august_hours = 298
+august_cost = august_hours * 0.65
+print("In August we spent: " + str(august_cost))
+
+def print_monthly_expense(month, hours):
+    print("In " + month + " we spent: " + str(hours * 0.65))
+
+print_monthly_expense("June", 243)
+print_monthly_expense("July", 325)
+print_monthly_expense("August", 298)
