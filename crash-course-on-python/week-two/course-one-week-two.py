96362ab54812da50cc506414658de8bf1526e5d9
Kyungrae <jeremiah.kim@hotmail.com>
2020-01-21 14:44:59 -0800

    returning value completed

--- crash-course-on-python/week-two/course-one-week-two.py
+++ crash-course-on-python/week-two/course-one-week-two.py
@@ -25,6 +25,7 @@ average = sum/amount
 print("The average size is: " + str(average))
 
 # Functions
+# Defining Functions
 """
 Flesh out the body of the print_seconds function so that it prints the total amount of seconds given the hours, minutes, and seconds function parameters. Remember that there are 3600 seconds in an hour and 60 seconds in a minute.
 """
@@ -32,3 +33,25 @@ def print_seconds(hours, minutes, seconds):
     print((hours * 60 * 60) + (minutes * 60) + seconds)
 
 print_seconds(1,2,3)
+
+# Returning Values
+"""
+Use the get_seconds function to work out the amount of seconds in 2 hours and 30 minutes, then add this number to the amount of seconds in 45 minutes and 15 seconds. Then print the result.
+"""
+def get_seconds(hours, minutes, seconds):
+    return 3600*hours + 60*minutes + seconds
+
+amount_a = get_seconds(2, 30, 0)
+amount_b = get_seconds(0, 45, 15)
+result = amount_a + amount_b
+print(result)
+
+# Example of returning multiple values from a function
+def convert_seconds(seconds):
+    hours = seconds // 3600
+    minutes = (seconds - hours * 3600) // 60
+    remaining_seconds = seconds - hours * 3600 - minutes * 60
+    return hours, minutes, remaining_seconds
+
+hours, minutes, seconds = convert_seconds(5000)
+print(hours, minutes, seconds)
