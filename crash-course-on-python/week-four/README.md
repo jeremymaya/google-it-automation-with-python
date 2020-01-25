9f5e6f1965b26f837bf80f4b50531dbe99bc7f96
Kyungrae <jeremiah.kim@hotmail.com>
2020-01-24 18:11:50 -0800

    strings, lists and dictionaries completed

--- crash-course-on-python/week-four/README.md
+++ crash-course-on-python/week-four/README.md
@@ -84,10 +84,33 @@ Since creating lists based on sequences is such a common task, Python provides a
 ---
 
 ## Dictionaries
+### What is a dictionary?
+**Dictionaries** take the form of pairs of keys and values to store data.
+* Dictionaries are mutable
+* Unlike lists, elements cannot be accessed using their position
 
+#### Definition
+x = {key1:value1, key2:value2}
+
+#### Operations
+* len(dictionary) - Returns the number of items in the dictionary
+* for key in dictionary - Iterates over each key in the dictionary
+* for key, value in dictionary.items() - Iterates over each key,value pair in the dictionary
+* if key in dictionary - Checks whether the key is in the dictionary
+* dictionary[key] - Accesses the item with key key of the dictionary
+* dictionary[key] = value - Sets the value associated with key
+* del dictionary[key] - Removes the item with key key from the dictionary
+
+#### Methods
+* dict.get(key, default) - Returns the element corresponding to key, or default if it's not present
+* dict.keys() - Returns a sequence containing the keys in the dictionary
+* dict.values() - Returns a sequence containing the values in the dictionary
+* dict.update(other_dictionary) - Updates the dictionary with the items coming from the other dictionary. Existing entries will be replaced; new entries will be added.
+* dict.clear() - Removes all the items of the dictionary
 ---
 
 ## Credit
 * [Coursera - Python Crash Course Week 4](https://www.coursera.org/learn/python-crash-course/home/week/4)
 * [Python Documentation - Common string operations](https://docs.python.org/3/library/string.html)
 * [Python Documentation - String Methods](https://docs.python.org/3/library/stdtypes.html#string-methods)
+* [Python Documentation - Dictionary](https://docs.python.org/3/library/stdtypes.html#mapping-types-dict)
--- crash-course-on-python/week-four/course-one-week-four.py
+++ crash-course-on-python/week-four/course-one-week-four.py
@@ -129,3 +129,31 @@ def skip_elements_list_comprehension(elements):
 
 print(skip_elements_list_comprehension(["a", "b", "c", "d", "e", "f", "g"])) # Should be ['a', 'c', 'e', 'g']
 print(skip_elements_list_comprehension(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']
+
+# Dictionaries
+# What is a dictionary?
+"""
+The "toc" dictionary represents the table of contents for a book. Fill in the blanks to do the following: 1) Add an entry for Epilogue on page 39. 2) Change the page number for Chapter 3 to 24. 3) Display the new dictionary contents. 4) Display True if there is Chapter 5, False if there isn't.
+"""
+toc = {"Introduction":1, "Chapter 1":4, "Chapter 2":11, "Chapter 3":25, "Chapter 4":30}
+toc["Epilogue"] = 39 # Epilogue starts on page 39
+toc["Chapter 3"] = 24 # Chapter 3 now starts on page 24
+print(toc) # What are the current contents?
+print("Chapter 5" in toc) # Is there a Chapter 5?
+
+# Iterating over the Contents of a Dictionary
+"""
+Complete the code to iterate through the keys and values of the cool_beasts dictionary. Remember that the items method returns a tuple of key, value for each element in the dictionary.
+"""
+cool_beasts = {"octopuses":"tentacles", "dolphins":"fins", "rhinos":"horns"}
+for beast, feature in cool_beasts.items():
+    print("{} have {}".format(beast, feature))
+
+# Dictionaries vs. Lists
+"""
+In Python, a dictionary can only hold a single value for a given key. To workaround this, our single value can be a list containing multiple values. Here we have a dictionary called "wardrobe" with items of clothing and their colors. Fill in the blanks to print a line for each item of clothing with each color, for example: "red shirt", "blue shirt", and so on.
+"""
+wardrobe = {"shirt":["red","blue","white"], "jeans":["blue","black"]}
+for cloth in wardrobe:
+    for color in wardrobe[cloth]:
+        print("{} {}".format(color, cloth))
