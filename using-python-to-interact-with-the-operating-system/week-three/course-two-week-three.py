8691a949bcec68a5610e559dce6715cf164f49e6
Kyungrae Kim <jeremiah.kim@hotmail.com>
2020-02-06 09:35:01 -0800

    regular expressions in action completed

--- using-python-to-interact-with-the-operating-system/week-three/course-two-week-three.py
+++ using-python-to-interact-with-the-operating-system/week-three/course-two-week-three.py
@@ -97,3 +97,20 @@ print(re.search(r"\.com", "mydomain.com"))
 # Use \b for word boundaries
 print(re.search(r"\w*", "This is an example"))
 print(re.search(r"\w*", "And_this_is_another"))
+
+# -----------------------------------------------------------------------------------------
+
+# Regular Expressions in Action
+# "Azerbaijan" returns "Azerbaija" because we did not specify the end 
+print(re.search(r"A.*a", "Argentina"))
+print(re.search(r"A.*a", "Azerbaijan"))
+
+# "Azerbaijan" returns None 
+print(re.search(r"^A.*a$", "Azerbaijan"))
+print(re.search(r"^A.*a$", "Australia"))
+
+pattern = r"^[a-zA-Z0-9_]*$"
+print(re.search(pattern, "this_is_a_valid_variable_name"))
+print(re.search(pattern, "this isn't a valid variable name"))
+print(re.search(pattern, "my_variable1"))
+print(re.search(pattern, "2my_variable1"))
