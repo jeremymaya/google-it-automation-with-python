bc14fed58ce575d4ce83d2c6a47c4afe2cf52afa
Kyungrae Kim <jeremiah.kim@hotmail.com>
2020-02-12 13:19:41 -0800

    additional test cases completed

--- using-python-to-interact-with-the-operating-system/week-five/rearrange_test.py
+++ using-python-to-interact-with-the-operating-system/week-five/rearrange_test.py
@@ -20,6 +20,16 @@ class TestRearrange(unittest.TestCase):
         testcase = ""
         expected = ""
         self.assertEqual(rearrange_name(testcase), expected)
+    
+    def test_double_name(self):
+        testcase = "Hopper, Grace M."
+        expected = "Grace M. Hopper"
+        self.assertEqual(rearrange_name(testcase), expected)
+
+    def test_one_name(self):
+        testcase = "Voltaire"
+        expected = "Voltaire"
+        self.assertEqual(rearrange_name(testcase), expected)
 
 # Runs the test
 unittest.main()
